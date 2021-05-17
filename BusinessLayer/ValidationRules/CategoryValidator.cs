1539bb4f1ae99ed2247bde669ba756011fcfd671
Katip <Katip@DESKTOP-6P2FP43>
2021-05-17 17:22:17 +0300

    Writer Validator Added

--- BusinessLayer/ValidationRules/CategoryValidator.cs
+++ BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -12,7 +12,7 @@ namespace BusinessLayer.ValidationRules
     {
         public CategoryValidator()
         {
-            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Kategori adını boş geçemezsiniz");
+            RuleFor(p => p.CategoryName).NotEmpty().WithMessage("Kategori adını boş geçemezsiniz");
             RuleFor(p => p.CategoryDescription).NotEmpty().WithMessage("Açıklamayı boş geçemezsiniz");
             RuleFor(p => p.CategoryName).MinimumLength(3).WithMessage("En az 3 karekter girişi yapın");
             RuleFor(p => p.CategoryName).MaximumLength(20).WithMessage("lütfen 20 karakterden fazla değer girişi yapmayın");
--- MvcProjectCamp/Controllers/WriterController.cs
+++ MvcProjectCamp/Controllers/WriterController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +18,32 @@ namespace MvcProjectCamp.Controllers
         {
             var writerValues = wm.GetList();
             return View(writerValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddWriter()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddWriter(Writer p)
+        {
+            WriterValidator writerValidator = new WriterValidator();
+            ValidationResult results = writerValidator.Validate(p);
+            if (results.IsValid)
+            {
+                wm.Add(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
         }
     }
 }
