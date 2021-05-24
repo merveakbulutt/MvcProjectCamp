3a8c226dc9938e5c8ac379c3be16b20d12c0b31f
Katip <Katip@DESKTOP-6P2FP43>
2021-05-24 03:58:13 +0300

    Heading operations added

---
+++ MvcProjectCamp/Controllers/HeadingController.cs
@@ -0,0 +1,55 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjectCamp.Controllers
+{
+    public class HeadingController : Controller
+    {
+
+        HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        CategoryManager cm= new CategoryManager(new EfCategoryDal());
+        WriterManager wm= new WriterManager(new EfWriterDal());
+
+        public ActionResult Index()
+        {
+            var headingvalues = hm.GetAll();
+            return View(headingvalues);
+        }
+
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            List<SelectListItem> valuecategory = (from x in cm.GetAll()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryId.ToString()
+                                                  }).ToList();
+
+            List<SelectListItem> valuewriter = (from x in wm.GetList()
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.WriterName + " " + x.WriterSurname,
+                                                    Value = x.WriterId.ToString()
+                                                }).ToList();
+
+            ViewBag.vlc = valuecategory;
+            ViewBag.vlw= valuewriter;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddHeading(Heading heading)
+        {
+            heading.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            hm.Add(heading);
+            return RedirectToAction("Index");
+        }
+    }
+}
