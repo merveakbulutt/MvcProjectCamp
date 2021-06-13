aa3614f32cc9ab8ceacbbfd628b66420a2e427af
Katip <Katip@DESKTOP-6P2FP43>
2021-06-13 21:30:04 +0300

    Gallery theme added.

---
+++ DataAccessLayer/Migrations/202106131708417_mig_addimage_file.cs
@@ -0,0 +1,27 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig_addimage_file : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ImageFiles",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ImageName = c.String(maxLength: 100),
+                        ImagePath = c.String(maxLength: 400),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.ImageFiles");
+        }
+    }
+}
